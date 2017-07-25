58060bf0c87fbe2ea8ed0db20cfbe70a1001517c
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-07-25 13:02:54 +0200

    bilard worki in progress...

--- Kwadracik/Bilard/Bilard.cs
+++ Kwadracik/Bilard/Bilard.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NumberTheory;
 
+/// <summary>
+/// http://main.edu.pl/pl/archive/pa/2005/bil
+/// </summary>
 namespace Bilard
 {
+    public enum Pocket {NIE, GL, GP, GS, DL, DP, DS};
+
     public class Bilard
     {
+        private bool IsMeshPoint(int sx, int sy, int x, int y)
+        {
+            return (x%(sx/2) == 0) && (y%sy == 0);
+        }
+
+        private Pocket SpecifyPocket(int sx, int sy, int x, int y)
+        {
+            
+        }
+
+        public Pocket CheckPocket(int sx, int sy, int px, int py, int wx, int wy)
+        {
+            //od razu w luzie
+            if (IsMeshPoint(sx, sy, px, py))
+            {
+                
+            }
+
+            //przypadki pionowe
+
+            //i pozostale
+            return Pocket.NIE;
+        }
     }
 }
--- Kwadracik/NBilard/NBilard.cs
+++ Kwadracik/NBilard/NBilard.cs
@@ -6,6 +6,8 @@ namespace NBilard
     [TestFixture]
     public class NBilard
     {
+        private Bilard.Bilard _pobj = new Bilard.Bilard();
+
         [Test]
         public void TestMethod1()
         {
