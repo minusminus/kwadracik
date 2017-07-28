33a62117398373b9926394f142788fcd7f0dcaa2
Michał Śmiechowski <Michal.Smiechowski@DSA.Polska>
2017-07-28 16:24:36 +0200

    bilard vertical and horizontal move

--- Kwadracik/Bilard/Bilard.cs
+++ Kwadracik/Bilard/Bilard.cs
@@ -32,15 +32,78 @@ namespace Bilard
             return res;
         }
 
+        /// <summary>
+        /// sx, sy - rozmiar stolu (sx zawsze parzyste)
+        /// px, py - polozenie kuli
+        /// wx, wy - wektor ruchu/uderzenia
+        /// </summary>
+        /// <param name="sx"></param>
+        /// <param name="sy"></param>
+        /// <param name="px"></param>
+        /// <param name="py"></param>
+        /// <param name="wx"></param>
+        /// <param name="wy"></param>
+        /// <returns></returns>
         public Pocket CheckPocket(int sx, int sy, int px, int py, int wx, int wy)
         {
             //od razu w luzie
             if (IsMeshPoint(sx, sy, px, py))
                 return SpecifyPocket(sx, sy, px, py);
 
+            //bez ruchu
+            if ((wx == 0) && (wy == 0)) return Pocket.NIE;
+
+            //przypadki poziome
+            if (wy == 0)
+            {
+                if (py == 0)
+                {
+                    if (wx > 0)
+                    {
+                        if (px < sx/2) return Pocket.DS;
+                        return Pocket.DP;
+                    }
+                    if (px > sx / 2) return Pocket.DS;
+                    return Pocket.DL;
+                }
+                if (py == sy)
+                {
+                    if (wx > 0)
+                    {
+                        if (px < sx / 2) return Pocket.GS;
+                        return Pocket.GP;
+                    }
+                    if (px > sx / 2) return Pocket.GS;
+                    return Pocket.GL;
+                }
+                return Pocket.NIE;
+            }
+
             //przypadki pionowe
+            if (wx == 0)
+            {
+                if (px == 0)
+                {
+                    if (wy > 0) return Pocket.GL;
+                    return Pocket.DL;
+                }
+                if (px == sx/2)
+                {
+                    if (wy > 0) return Pocket.GS;
+                    return Pocket.DS;
+                }
+                if (px == sx)
+                {
+                    if (wy > 0) return Pocket.GP;
+                    return Pocket.DP;
+                }
+                return Pocket.NIE;
+            }
 
             //i pozostale
+            double a = (double) wy/(double) wx;
+            double b = py - a*px;   
+
             return Pocket.NIE;
         }
     }
--- Kwadracik/NBilard/NBilard.cs
+++ Kwadracik/NBilard/NBilard.cs
@@ -25,8 +25,60 @@ namespace NBilard
         }
 
         [Test]
-        public void TestMethod1()
+        public void NotInPocketNotMoving()
         {
+            _pobj.CheckPocket(4, 2, 1, 1, 0, 0).ShouldBe(Pocket.NIE);
+        }
+
+        [Test]
+        public void IniitallyInPocket()
+        {
+            const int sx = 4;
+            const int sy = 2;
+
+            _pobj.CheckPocket(sx, sy, 0, 0, 1, 1).ShouldBe(Pocket.DL);
+            _pobj.CheckPocket(sx, sy, sx, 0, 1, 1).ShouldBe(Pocket.DP);
+            _pobj.CheckPocket(sx, sy, sx/2, 0, 1, 1).ShouldBe(Pocket.DS);
+            _pobj.CheckPocket(sx, sy, 0, sy, 1, 1).ShouldBe(Pocket.GL);
+            _pobj.CheckPocket(sx, sy, sx, sy, 1, 1).ShouldBe(Pocket.GP);
+            _pobj.CheckPocket(sx, sy, sx/2, sy, 1, 1).ShouldBe(Pocket.GS);
+        }
+
+        [Test]
+        public void HorizontalMove()
+        {
+            const int sx = 4;
+            const int sy = 2;
+
+            _pobj.CheckPocket(sx, sy, 1, 1, 1, 0).ShouldBe(Pocket.NIE);
+            _pobj.CheckPocket(sx, sy, 1, 1, -1, 0).ShouldBe(Pocket.NIE);
+
+            _pobj.CheckPocket(sx, sy, 1, 0, 1, 0).ShouldBe(Pocket.DS);
+            _pobj.CheckPocket(sx, sy, 3, 0, 1, 0).ShouldBe(Pocket.DP);
+            _pobj.CheckPocket(sx, sy, 1, 0, -1, 0).ShouldBe(Pocket.DL);
+            _pobj.CheckPocket(sx, sy, 3, 0, -1, 0).ShouldBe(Pocket.DS);
+
+            _pobj.CheckPocket(sx, sy, 1, sy, 1, 0).ShouldBe(Pocket.GS);
+            _pobj.CheckPocket(sx, sy, 3, sy, 1, 0).ShouldBe(Pocket.GP);
+            _pobj.CheckPocket(sx, sy, 1, sy, -1, 0).ShouldBe(Pocket.GL);
+            _pobj.CheckPocket(sx, sy, 3, sy, -1, 0).ShouldBe(Pocket.GS);
+        }
+
+        [Test]
+        public void VerticalMove()
+        {
+            const int sx = 4;
+            const int sy = 2;
+
+            _pobj.CheckPocket(sx, sy, 1, 1, 0, 1).ShouldBe(Pocket.NIE);
+            _pobj.CheckPocket(sx, sy, 1, 1, 0, -1).ShouldBe(Pocket.NIE);
+
+            _pobj.CheckPocket(sx, sy, 0, 1, 0, 1).ShouldBe(Pocket.GL);
+            _pobj.CheckPocket(sx, sy, 0, 1, 0, -1).ShouldBe(Pocket.DL);
+            _pobj.CheckPocket(sx, sy, sx/2, 1, 0, 1).ShouldBe(Pocket.GS);
+            _pobj.CheckPocket(sx, sy, sx/2, 1, 0, -1).ShouldBe(Pocket.DS);
+            _pobj.CheckPocket(sx, sy, sx, 1, 0, 1).ShouldBe(Pocket.GP);
+            _pobj.CheckPocket(sx, sy, sx, 1, 0, -1).ShouldBe(Pocket.DP);
         }
     }
 }
